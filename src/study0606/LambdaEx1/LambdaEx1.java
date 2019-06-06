8e1deea3f7da8239b1c70eec994fd39282feb59b
gracelove91 <govlmo91@gmail.com>
2019-06-06 19:25:09 +0900

    study0606 update

---
+++ src/study0606/LambdaEx1/LambdaEx1.java
@@ -0,0 +1,20 @@
+package study0606.LambdaEx1;
+@FunctionalInterface
+interface MyFunction{
+	void myMethod1();
+}
+public class LambdaEx1 {
+	public static void main(String[] args) {
+		MyFunction f = () -> System.out.println("메메");
+		f.myMethod1();
+		
+		excute(f);
+		
+		Object o = (MyFunction)(() -> {});
+		String s = (Object)((MyFunction)(() -> {})).toString();
+	}
+	
+	static void excute(MyFunction f) {
+		f.myMethod1();
+	}
+}
---
+++ src/study0606/LambdaEx3/LambdaEx3.java
@@ -0,0 +1,21 @@
+package study0606.LambdaEx3;
+@FunctionalInterface
+interface MyFunction{
+	void myMethod();
+}
+public class LambdaEx3 {
+	int val = 10; // LambdaEx3.this.val
+	
+	class Inner{
+		int val = 20; // this.val
+		
+		void method(int i) { //void method(final int i)
+			int val = 30; //final int val = 30;
+			i = 20;	
+			
+			MyFunction f = () -> {
+				i = 50;
+			};
+		}
+	}
+}
