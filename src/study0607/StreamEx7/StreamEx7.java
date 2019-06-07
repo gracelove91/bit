fa5bab129cf64aaddbdb081774138da16065bf4e
gracelove91 <govlmo91@gmail.com>
2019-06-07 21:13:32 +0900

    study0607 update

---
+++ src/study0607/StreamEx7/StreamEx7.java
@@ -0,0 +1,95 @@
+package study0607.StreamEx7;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.*;
+import static java.util.Comparator.*;
+
+class Student{
+	String name;
+	boolean isMale;
+	int hak;
+	int ban;
+	int score;
+	
+	public Student(String name, boolean isMale, int hak, int ban, int score) {
+		this.name = name;
+		this.isMale = isMale;
+		this.hak = hak;
+		this.ban = ban;
+		this.score = score;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public boolean isMale() {
+		return isMale;
+	}
+
+	public int getHak() {
+		return hak;
+	}
+
+	public int getBan() {
+		return ban;
+	}
+
+	public int getScore() {
+		return score;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("[%s, %s, %d학년 %d반, %3d ]", name, isMale ? "남":"여", hak, ban, score);
+	}
+	
+	//groupingBy()에서 사용.
+	enum Level { HIGH, MID, LOW } //성적을 상, 중, 하 세단계로 분류.
+	
+	
+}
+public class StreamEx7 {
+	public static void main(String[] args) {
+		Student[] stuArr = {
+				new Student("나자바", true, 1, 1, 300),
+				new Student("김지미", false, 1, 1, 250),
+				new Student("김자바", true, 1, 1, 200),
+				new Student("이지미", false, 1, 2, 150),
+				new Student("남자바", true, 1, 2, 100),
+				new Student("안지미", false, 1, 2, 50),
+				new Student("황지미", false, 1, 3, 100),
+				new Student("강지미", false, 1, 3, 150),
+				new Student("이자바", true, 1, 3, 200),
+				
+				new Student("나자바", true, 1, 1, 300),
+				new Student("김지미", false, 1, 1, 250),
+				new Student("김자바", true, 1, 1, 200),
+				new Student("이지미", false, 1, 2, 150),
+				new Student("남자바", true, 1, 2, 100),
+				new Student("안지미", false, 1, 2, 50),
+				new Student("황지미", false, 1, 3, 100),
+				new Student("강지미", false, 1, 3, 150),
+				new Student("이자바", true, 1, 3, 200),
+		};
+		
+		System.out.printf("1. 단순분할(성별로 분할) %n");
+		Map<Boolean, List<Student>> stuBySex = Stream.of(stuArr).collect(partitioningBy(Student::isMale));
+		
+		List<Student> maleStudent = stuBySex.get(true);
+		List<Student> femaleStudent = stuBySex.get(false);
+		
+		for(Student s  : maleStudent) System.out.println(s);
+		for(Student s : femaleStudent) System.out.println(s);
+		
+		System.out.println("%n2. 단순분할 + 통계(성별 학생수)%n");
+		Map<Boolean, Long> stuNumBySex = Stream.of(stuArr).collect(partitioningBy(Student::isMale, counting()));
+		
+		System.out.println("남학생 수 : "+stuNumBySex.get(true));
+		System.out.println("여학생 수 : "+stuNumBySex.get(false));
+		
+		
+	}
+}
